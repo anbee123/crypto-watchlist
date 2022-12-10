b80144c021da73dfacfe0141c82e609109ce7649
An Bui <anhipchipchip@gmail.com>
2022-12-09 22:20:41 -0500

    Added in Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,27 @@
 
+import { AppBar, Toolbar, Typography, Select, MenuItem } from "@mui/material";
+import { Container } from "@mui/system";
 import React from "react";
 
  const Header =() =>{
     return (
-<div>header</div>
+<AppBar color= 'transparent' position='static'>
+<Container>
+    <Toolbar>
+        <Typography> Crypto Tracker </Typography>
+        <Select 
+        variant="outlined"
+        style={{
+            width:100,
+            height: 40,
+            marginLeft: 15,
+        }}>
+<MenuItem value={'USD'}>USD</MenuItem>
+<MenuItem value={'EUR'}>EUR</MenuItem>
+        </Select>
+    </Toolbar>
+</Container>
+</AppBar>
     )
  }
  export default Header;
