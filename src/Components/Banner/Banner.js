f598c21b2a5319d815316d90127a26ffb49d25b9
An Bui <anhipchipchip@gmail.com>
2022-12-23 22:06:06 -0500

    working on Banner.js

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,12 +1,22 @@
 
+import { Typography } from '@mui/material';
+import { Container } from '@mui/system';
 import { makeStyles} from 'tss-react/mui'
 
 
 const useStyles = makeStyles()(() => {
     return {
-      App: {
-        backgroundImage: 'url(./bannerpic.png)',
+      banner: {
+        backgroundImage: 'url(./bannerphto.jpeg)',
       },
+      bannerContent: {
+        height: 400,
+    display: 'flex',
+    flexDirection: 'column',
+    paddingTop: 25,
+    justifyContent: 'space-around',        
+
+      }
     };
   });
   
@@ -14,6 +24,19 @@ const Banner = ()=>{
     const { classes } = useStyles();
     return (
         <div className={classes.banner}>
+            <Container className={classes.bannerContent}>
+              <div className={classes.tagline}>
+                <Typography
+                variant='h2'
+                style={{
+                  fontWeight: 'bold',
+                  marginBottom: 15,
+                  fontFamily: 'Montserrat',
+                }} >
+                  Crypto Tracker
+                </Typography>
+              </div>
+            </Container>
         </div>
     )
 }
