8e7be0338342e6fc8ed3c416baf4b4a06696be33
An Bui <anhipchipchip@gmail.com>
2023-01-31 12:07:44 -0500

    coinpage + chart

--- src/Components/Banner/CoinsTable.js
+++ src/Components/Banner/CoinsTable.js
@@ -15,21 +15,21 @@ import { makeStyles } from 'tss-react/mui'
 export function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   }
-  const useStyles = makeStyles()(() => {
-    return {
+  const useStyles = makeStyles({
+
         row: {
             backgroundColor: "#16171a",
             cursor: "pointer",
-            "&:hover": {
+            '&:hover': {
               backgroundColor: "#131111",
             },
             fontFamily: "Montserrat",
           },
           pagination: {
-            "& .MuiPaginationItem-root": {
-              color: "gold",
+            '& .MuiPaginationItem-root': {
+              color: "white",
             },
-          },
+          
   }});
 
 
@@ -73,7 +73,7 @@ const CoinsTable = () => {
             coin.symbol.toLowerCase().includes(search)
         );
       };
-
+      
 
     return (
         <ThemeProvider theme={darkTheme}>
